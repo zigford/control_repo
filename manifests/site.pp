45c81d37f4d793d5f790717aee0ca28fcffccee8
Jesse Harris <zigford@gmail.com>
2019-11-16 22:35:11 +1000

    added master to site

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-    file {'/root/README':
-        ensure  => file,
-        content => 'This is a stupid readme',
-        owner   => root,
-    }
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
