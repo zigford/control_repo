53292e536eccb24c462ed682fe73ca5cc7bf0470
Jesse Harris <zigford@gmail.com>
2019-12-02 09:18:38 +1000

    service added

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,4 +17,8 @@ class minecraft {
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
+  server {'minecraft':
+    ensure => running,
+    enable => true,
+  }
 }
