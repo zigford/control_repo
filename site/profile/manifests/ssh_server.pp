b7a87af211d7495c51761892fd7016a708b4aa27
Jesse Harris <zigford@gmail.com>
2019-12-01 16:41:58 +1000

    fixed ssh key

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuaoZXNK61FsXQEXRB5LkW+VF6oGXGGnJhHYH5n69RKn8wAb1IYxJ98/pRm5c4UtS263nb7tPxk4m/+V11cMlbEVaIa/XhxrXNF/q3Kx0EH8bfvsc+DSMoRTxtEQYFgoH3w1HVBghX10N4VuucA2Wl4L+vfM3hCYih+phzMZE2uK24R47ZiG5H1XCWZOiQrXpvedqBkYjJsPbYy3w0HiDWDwoRWN8gf0YZKMsQbbG6/RRRdIm/Bew/Gp6NkFJUm8og67m3gCZo3vQGZsRj0Zr+gDKLU4Tx+uHQ5uezZtXri/37lmgU92GGLvIM/ekmeM/Um8MmOCh/vj018fxcFgU5 root@master.puppet.vm',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCuaoZXNK61FsXQEXRB5LkW+VF6oGXGGnJhHYH5n69RKn8wAb1IYxJ98/pRm5c4UtS263nb7tPxk4m/+V11cMlbEVaIa/XhxrXNF/q3Kx0EH8bfvsc+DSMoRTxtEQYFgoH3w1HVBghX10N4VuucA2Wl4L+vfM3hCYih+phzMZE2uK24R47ZiG5H1XCWZOiQrXpvedqBkYjJsPbYy3w0HiDWDwoRWN8gf0YZKMsQbbG6/RRRdIm/Bew/Gp6NkFJUm8og67m3gCZo3vQGZsRj0Zr+gDKLU4Tx+uHQ5uezZtXri/37lmgU92GGLvIM/ekmeM/Um8MmOCh/vj018fxcFgU5 root@master.puppet.vm',
   }
 }
