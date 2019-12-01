2e19794da5507a2f529e0ea0136726aae788e67a
Jesse Harris <zigford@gmail.com>
2019-12-02 09:12:10 +1000

    new module

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,20 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar'
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eular.exe':
+    ensure => file,
+    conent => 'eula=true'
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+}
