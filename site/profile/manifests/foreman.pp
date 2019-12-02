2f0b74286d428d9563fcf30a2967d5b96d2a0266
Jesse Harris <zigford@gmail.com>
2019-12-02 22:04:27 +1000

    mod deps for foreman

--- site/profile/manifests/foreman.pp
+++ site/profile/manifests/foreman.pp
@@ -1,7 +1,8 @@
 class profile::foreman{
     include epel
-    yum::install { 'foreman-installer':
-      ensure => present,
-      source => 'https://yum.theforeman.org/releases/1.23/el7/x86_64/foreman-release.rpm',
-    }
+    include foreman
+    #    yum::install { 'foreman-installer':
+    #      ensure => present,
+    #      source => 'https://yum.theforeman.org/releases/1.23/el7/x86_64/foreman-release.rpm',
+    #    }
 }
