bde4409f1c5e33f891fbecbc408c596e56beeb7d
Jesse Harris <zigford@gmail.com>
2019-12-02 10:58:50 +1000

    typo in eula

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -9,7 +9,7 @@ class minecraft {
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eular.exe':
+  file {'/opt/minecraft/eula.txt':
     ensure  => file,
     content => 'eula=true'
   }
