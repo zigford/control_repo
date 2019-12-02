871410e08fb5dd26c000a78b3b49002bfbe4be30
Jesse Harris <zigford@gmail.com>
2019-12-02 22:57:40 +1000

    foreman proxy deps

--- site/profile/manifests/foreman.pp
+++ site/profile/manifests/foreman.pp
@@ -1,5 +1,15 @@
 class profile::foreman{
     include foreman
+    include foreman_proxy
+    class {'::foreman_proxy':
+      puppet   => true,
+      puppetca => true,
+      tftp     => false,
+      dhcp     => false,
+      dns      => false,
+      bmc      => false,
+      realm    => false,
+    }
     #    yum::install { 'foreman-installer':
     #      ensure => present,
     #      source => 'https://yum.theforeman.org/releases/1.23/el7/x86_64/foreman-release.rpm',
