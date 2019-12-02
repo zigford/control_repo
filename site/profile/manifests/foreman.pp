04e20f940ca2cb5f24c7c301bd46ddf33c037751
Jesse Harris <zigford@gmail.com>
2019-12-02 21:41:19 +1000

    source not right

--- site/profile/manifests/foreman.pp
+++ site/profile/manifests/foreman.pp
@@ -2,6 +2,6 @@ class profile::foreman{
     include epel
     yum::install { 'foreman-installer':
       ensure => present,
-      source => 'sudo yum -y install https://yum.theforeman.org/releases/1.23/el7/x86_64/foreman-release.rpm',
+      source => 'https://yum.theforeman.org/releases/1.23/el7/x86_64/foreman-release.rpm',
     }
 }
