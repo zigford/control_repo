ff1fd3cd8b01b5e14a1e2d57cf8ed7dec9fc2371
Jesse Harris <zigford@gmail.com>
2019-12-02 21:39:43 +1000

    install rpm foreman

--- site/profile/manifests/foreman.pp
+++ site/profile/manifests/foreman.pp
@@ -1,3 +1,7 @@
 class profile::foreman{
     include epel
+    yum::install { 'foreman-installer':
+      ensure => present,
+      source => 'sudo yum -y install https://yum.theforeman.org/releases/1.23/el7/x86_64/foreman-release.rpm',
+    }
 }
