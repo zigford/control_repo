78468c9aa3d9edea972168af1d3c4d4820747e14
Jesse Harris <zigford@gmail.com>
2019-12-02 20:48:58 +1000

    updated module

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,8 @@ class minecraft {
   }
   file {'/opt/minecraft/server.jar':
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar'
+    source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
+    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
@@ -18,7 +19,8 @@ class minecraft {
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
   service {'minecraft':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.server']],
   }
 }
